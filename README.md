e036eac29efca42d94e4a9f8fd190ae202fea816
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-20 15:33:28 +0600

    lesson 3 completed

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ https://mobarak-ali.github.io/module12/
 ## 12-1 Module overview, Run JavaScript in VSCode
 
 ## 12-2 What is variable, declare a variable, see output
+
+## 12-3 Variable type, Numeric, String, Boolean
+
+## 12-4 variable name naming convention and best practice
---
+++ lesson03.js
@@ -0,0 +1,19 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+console.log(typeof bananaPrice);
+
+var seenAfter = 21;
+
+console.log(seenAfter);
+console.log(typeof seenAfter);
+
+var name = "KalimUddin Zukerburg";
+console.log(name);
+
+console.log(typeof name);
+
+
+var  isHot = true;
+var isRich = false;
+
+console.log(typeof isHot, typeof isRich)
