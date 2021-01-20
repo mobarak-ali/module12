b2ce0f009fe5ac42db87e23668efb77c04d380b1
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-20 15:44:53 +0600

    lesson 04 is OK

---
+++ lesson04.js
@@ -0,0 +1,13 @@
+var name = 'Hero Alam';
+Myname = 'Hero Abdul';
+console.log(name);
+
+// Case Sensetive
+
+// camelCase
+var myVariable = "Value";
+
+// type meaningful name as variable
+
+var u // incorrect
+var user // Correct
