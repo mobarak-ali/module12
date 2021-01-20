f546289e010aa04011e75275bed897eddf5af271
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-20 20:30:12 +0600

    lesson 09 OK

--- lesson09.js
+++ lesson09.js
@@ -1,1 +1,12 @@
 // 12-9 Make conditional decision, if-else, comparison
+var biscuitPrice = 12;
+if (biscuitPrice < 10) {
+    console.log("I will eat Biscuit");
+} else {
+    console.log("Khali cha is better");
+}
+
+console.log(biscuitPrice < 12);  // is less than
+console.log(biscuitPrice == 12); // is equal to
+console.log(biscuitPrice > 12);  // is greater than
+console.log(biscuitPrice >= 12); // is less than or Equal to
